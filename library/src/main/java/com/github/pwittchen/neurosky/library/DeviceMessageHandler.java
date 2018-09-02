97cd5fd66d71fd38a3f90808603780f23fb50ea8
Piotr Wittchen <piotr@wittchen.io>
2018-09-02 21:08:50 +0200

    adding abstract class ExtendedDeviceMessageListener

--- library/src/main/java/com/github/pwittchen/neurosky/library/DeviceMessageHandler.java
+++ library/src/main/java/com/github/pwittchen/neurosky/library/DeviceMessageHandler.java
@@ -3,14 +3,7 @@ package com.github.pwittchen.neurosky.library;
 import android.os.Handler;
 import android.os.Message;
 import com.github.pwittchen.neurosky.library.listeners.DeviceMessageListener;
-import com.github.pwittchen.neurosky.library.message.BrainWave;
-import com.github.pwittchen.neurosky.library.message.Signal;
-import com.github.pwittchen.neurosky.library.message.State;
-import com.neurosky.thinkgear.TGEegPower;
-import java.util.HashSet;
-import java.util.Set;
 
-//TODO: prepare another handler with more specific interface for different signals and states
 public class DeviceMessageHandler extends Handler {
 
   private DeviceMessageListener listener;
@@ -25,48 +18,5 @@ public class DeviceMessageHandler extends Handler {
     if (listener != null) {
       listener.listen(message);
     }
-
-    //TODO: utilize values below - e.g. create an object containing Signal, State and BrainWave
-    //getSignal(message);
-    //getState(message);
-    //getBrainWaves(message);
-  }
-
-  private Signal getSignal(Message message) {
-    for (Signal signal : Signal.values()) {
-      if (message.what == signal.getType()) {
-        return signal.value(message.arg1);
-      }
-    }
-
-    return Signal.UNKNOWN;
-  }
-
-  private State getState(Message message) {
-    if (message.what == Signal.STATE_CHANGE.getType()) {
-      for (State state : State.values()) {
-        if (message.arg1 == state.getType()) {
-          return state;
-        }
-      }
-    }
-    return State.UNKNOWN;
-  }
-
-  private Set<BrainWave> getBrainWaves(Message message) {
-    Set<BrainWave> brainWaves = new HashSet<>();
-
-    if (message.what == Signal.EEG_POWER.getType()) {
-      TGEegPower eegPower = (TGEegPower) message.obj;
-      brainWaves.add(BrainWave.DELTA.value(eegPower.delta));
-      brainWaves.add(BrainWave.THETA.value(eegPower.theta));
-      brainWaves.add(BrainWave.LOW_ALPHA.value(eegPower.lowAlpha));
-      brainWaves.add(BrainWave.HIGH_ALPHA.value(eegPower.highAlpha));
-      brainWaves.add(BrainWave.LOW_BETA.value(eegPower.lowBeta));
-      brainWaves.add(BrainWave.LOW_GAMMA.value(eegPower.lowGamma));
-      brainWaves.add(BrainWave.MID_GAMMA.value(eegPower.midGamma));
-    }
-
-    return brainWaves;
   }
 }
---
+++ library/src/main/java/com/github/pwittchen/neurosky/library/listeners/ExtendedDeviceMessageListener.java
@@ -0,0 +1,74 @@
+package com.github.pwittchen.neurosky.library.listeners;
+
+import android.os.Message;
+import com.github.pwittchen.neurosky.library.message.BrainWave;
+import com.github.pwittchen.neurosky.library.message.Signal;
+import com.github.pwittchen.neurosky.library.message.State;
+import com.neurosky.thinkgear.TGEegPower;
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class ExtendedDeviceMessageListener implements DeviceMessageListener {
+
+  @Override public void listen(Message message) {
+    State state = getState(message);
+    if (state != State.UNKNOWN) {
+      onStateChange(state);
+    }
+
+    Signal signal = getSignal(message);
+    if (signal != Signal.UNKNOWN) {
+      onSignalChange(signal);
+    }
+
+    Set<BrainWave> brainWaves = getBrainWaves(message);
+    if (!brainWaves.isEmpty()) {
+      onBrainWavesChange(brainWaves);
+    }
+  }
+
+  public abstract void onStateChange(State state);
+
+  public abstract void onSignalChange(Signal signal);
+
+  public abstract void onBrainWavesChange(Set<BrainWave> brainWaves);
+
+  protected Signal getSignal(Message message) {
+    for (Signal signal : Signal.values()) {
+      if (message.what == signal.getType()) {
+        return signal.value(message.arg1);
+      }
+    }
+
+    return Signal.UNKNOWN;
+  }
+
+  protected State getState(Message message) {
+    if (message.what == Signal.STATE_CHANGE.getType()) {
+      for (State state : State.values()) {
+        if (message.arg1 == state.getType()) {
+          return state;
+        }
+      }
+    }
+
+    return State.UNKNOWN;
+  }
+
+  protected Set<BrainWave> getBrainWaves(Message message) {
+    final Set<BrainWave> brainWaves = new HashSet<>();
+
+    if (message.what == Signal.EEG_POWER.getType()) {
+      TGEegPower eegPower = (TGEegPower) message.obj;
+      brainWaves.add(BrainWave.DELTA.value(eegPower.delta));
+      brainWaves.add(BrainWave.THETA.value(eegPower.theta));
+      brainWaves.add(BrainWave.LOW_ALPHA.value(eegPower.lowAlpha));
+      brainWaves.add(BrainWave.HIGH_ALPHA.value(eegPower.highAlpha));
+      brainWaves.add(BrainWave.LOW_BETA.value(eegPower.lowBeta));
+      brainWaves.add(BrainWave.LOW_GAMMA.value(eegPower.lowGamma));
+      brainWaves.add(BrainWave.MID_GAMMA.value(eegPower.midGamma));
+    }
+
+    return brainWaves;
+  }
+}
