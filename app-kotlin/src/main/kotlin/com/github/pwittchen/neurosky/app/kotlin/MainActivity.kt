6f69973b1790a255b19cb8265ef9fe58f7014831
Piotr Wittchen <piotr@wittchen.io>
2018-09-03 20:45:31 +0200

    updating config of the sample kotlin app and adding app-rxkotlin module

---
+++ app-kotlin/src/main/kotlin/com/github/pwittchen/neurosky/app/kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.pwittchen.neurosky.app.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.pwittchen.neurosky.app.R.layout.activity_main
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(activity_main)
+  }
+}
--- app-rxkotlin/src/main/kotlin/com/github/pwittchen/neurosky/app/rxkotlin/MainActivity.kt
+++ app-rxkotlin/src/main/kotlin/com/github/pwittchen/neurosky/app/rxkotlin/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.github.pwittchen.neurosky.app
+package com.github.pwittchen.neurosky.app.rxkotlin
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.github.pwittchen.neurosky.app.R.layout.activity_main
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(activity_main)
+    setContentView(R.layout.activity_main)
   }
 }
